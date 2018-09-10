62f04aa5fe3370531101c257d70d7e45c9e8e29a
Mohammad Umar <umarsoft.java@gmail.com>
2018-09-11 03:45:54 +0530

    updated

--- zone-service/config/db.js
+++ zone-service/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
     rethinkdb: {
-        host: process.env.DB_HOST ||"localhost", // for docker 
+        host: process.env.DB_HOST ||"18.191.39.211", // for docker 
         port: process.env.DB_PORT || 28015,
         authKey: "",
         db: "zoneapp",
